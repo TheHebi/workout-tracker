6f305e867c8391dd482260a9ba3009d9f93fa11a
TheHebi <nturcotte8@live.com>
2021-08-16 16:04:56 -0700

    deploying

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const workoutRoutes = require("./workout")
+const workoutRoutes = require("./workout");
 const homeRoutes = require("./home");
 
-router.use("/api",workoutRoutes);
+router.use("/api", workoutRoutes);
 router.use("/", homeRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const routes = require('./controllers');
+const routes = require("./controllers");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,9 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-app.use('/',routes);
+app.use("/", routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
